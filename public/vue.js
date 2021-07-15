cdb44212a45dea86df605b25060e4796a51e6399
Raider144 <lawrence.isaac970@gmail.com>
2021-07-15 10:09:16 -0600

    adding logic

--- public/vue.js
+++ public/vue.js
@@ -3,11 +3,30 @@ var app = new Vue({
 
 
   data: {
-      
+    isActive: true,
+
   },
 
   
   methods: {
+    displayForm: function() {
+      // on load of the DOM :
+        // set loginForm block to isActive: false,
+        // set landingContainer to isActive: true,
+        // set createAccountForm to isActive: false,
+      // on click of sign in button :
+        // set loginForm block to isActive: true,
+        // set landingContainer to isActive: false,
+        // set createAccountForm to isActive: false,
+      // onclick of sign UP button :
+        // set loginForm block to isActive: false,
+        // set landingContainer to isActive: false,
+        // set createAccountForm to isActive: true,
+        
+    },
+    submitForm: function() {
+
+    }
 
   },
 
