8bd11915c92e03874cb69456d3ee01cc1eb8beab
Eliza Darger <elizadarger@Elizas-MacBook-Air.local>
2021-07-15 10:05:29 -0600

    created tentative data model and started addNewRoute

--- public/vue.js
+++ public/vue.js
@@ -1,9 +1,16 @@
 var app = new Vue({
   el: "#vue-app-wrapper",
 
-  data: {},
+  data: {
+    routes: [],
+    new_from_location: "",
+    new_start_mileage: "",
+    new_to_location: "",
+    new_end_mileage: "",
+  },
 
   methods: {
+    //Untested.
     getRoutes: function () {
       fetch(`${url}/routes`).then(function (response) {
         response.json().then(function (data) {
@@ -12,6 +19,15 @@ var app = new Vue({
         });
       });
     },
+  },
+
+  addNewRoute: function () {
+    var request_body = {
+      from_location: this.new_from_location,
+      start_mileage: this.new_start_mileage,
+      to_location: this.new_start_location,
+      end_mileage: this.new_end_location,
+    };
   },
 
   created: {},
