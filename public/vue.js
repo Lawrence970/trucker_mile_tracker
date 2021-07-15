039db2ba5c4b6e38f56f33ba3746a311e0e14ab0
Eliza Darger <elizadarger@Elizas-MacBook-Air.local>
2021-07-15 10:29:15 -0600

    addNewRoute created, untested

--- public/vue.js
+++ public/vue.js
@@ -12,24 +12,21 @@ var app = new Vue({
   },
 
   methods: {
-    displayForm: function() {
+    displayForm: function () {
       // on load of the DOM :
-        // set loginForm block to isActive: false,
-        // set landingContainer to isActive: true,
-        // set createAccountForm to isActive: false,
+      // set loginForm block to isActive: false,
+      // set landingContainer to isActive: true,
+      // set createAccountForm to isActive: false,
       // on click of sign in button :
-        // set loginForm block to isActive: true,
-        // set landingContainer to isActive: false,
-        // set createAccountForm to isActive: false,
+      // set loginForm block to isActive: true,
+      // set landingContainer to isActive: false,
+      // set createAccountForm to isActive: false,
       // onclick of sign UP button :
-        // set loginForm block to isActive: false,
-        // set landingContainer to isActive: false,
-        // set createAccountForm to isActive: true,
-        
+      // set loginForm block to isActive: false,
+      // set landingContainer to isActive: false,
+      // set createAccountForm to isActive: true,
     },
-    submitForm: function() {
-
-    },
+    submitForm: function () {},
 
     //Untested.
     getRoutes: function () {
@@ -49,6 +46,26 @@ var app = new Vue({
       to_location: this.new_start_location,
       end_mileage: this.new_end_location,
     };
+    fetch(`${url}/routes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request_body),
+    }).then(function (response) {
+      console.log(request_body);
+      if (response.status == 400) {
+        response.json().then(function (data) {
+          alert(data.msg);
+        });
+      } else if (response.status == 201) {
+        (app.new_from_location = ""),
+          (app.new_start_mileage = ""),
+          (app.new_to_location = ""),
+          (app.new_end_mileage = "");
+        app.getRoutes();
+      }
+    });
   },
 
   created: {},
